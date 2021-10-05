99c2a6a76fecb700ae3b95f1045f5ecf61fc040b
jMisch44 <jennimisch@gmail.com>
2021-10-05 18:15:44 -0400

    added cars seed and run seed script

--- api/server.js
+++ api/server.js
@@ -11,7 +11,7 @@ server.use('*', (req, res) => {
 
 server.use(errorHandling)
 
-function errorHandling (err, req, res, next) {
+function errorHandling (err, req, res, next) { // eslint-disable-line
     res.status(err.status || 500).json({
         message: err.message,
         stack: err.stack,
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,32 @@
-// STRETCH
+exports.seed = async function(knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert([
+        {
+            vin: "SCBLC37F85CX10207",
+            make: "Bentley",
+            model: "Arnage",
+            mileage: 1000,
+            title: "clean",
+            transmission: "manual",
+
+        },
+        {
+            vin: "2FTDF18W5VCA88039",
+            make: "Ford",
+            model: "F 150",
+            mileage: 100000,
+            title: "clean",
+            transmission: "automatic",
+
+        },
+        {
+            vin: "4JGBB8GB4BA662410",
+            make: "Mercedes Benz",
+            model: "2011 ML",
+            mileage: 10000,
+            title: "clean",
+            transmission: "automatic",
+
+        }
+    ])
+}
