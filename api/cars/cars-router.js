7d880b0f03b4eaa3bdae5e9ca5cbb1e0eb006a13
jMisch44 <jennimisch@gmail.com>
2021-10-05 15:02:34 -0400

    car router and server set up

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,28 @@
-// DO YOUR MAGIC
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    try {
+        res.json('get cars')
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.get('/:id', (req, res, next) => {
+    try {
+        res.json('get car by id')
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.post('/', (req, res, next) => {
+    try {
+        res.json('create new car')
+    } catch (err) {
+        next(err)
+    }
+})
+
+module.exports = router
